94d388f8574bbd8b940a53083324cca423b2ef3b
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-31 17:53:40 +0300

    updated: create navigation direction actions

--- app/src/main/java/com/ismailmesutmujde/javanavigation/FirstFragment.java
+++ app/src/main/java/com/ismailmesutmujde/javanavigation/FirstFragment.java
@@ -2,11 +2,16 @@ package com.ismailmesutmujde.javanavigation;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FirstFragment extends Fragment {
 
@@ -30,7 +35,22 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
-    public void goToSecond(View view) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        Button button  = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSecond(view);
+            }
+        });
+
+    }
+
+    public void goToSecond(View view) {
+        NavDirections action = FirstFragmentDirections.actionFirstFragmentToSecondFragment();
+        Navigation.findNavController(view).navigate(action);
     }
 }
--- app/src/main/java/com/ismailmesutmujde/javanavigation/SecondFragment.java
+++ app/src/main/java/com/ismailmesutmujde/javanavigation/SecondFragment.java
@@ -2,11 +2,17 @@ package com.ismailmesutmujde.javanavigation;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class SecondFragment extends Fragment {
@@ -30,7 +36,22 @@ public class SecondFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
-    public void goToFirst(View view) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button button = view.findViewById(R.id.button2);
+        TextView textView = view.findViewById(R.id.textView);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFirst(view);
+            }
+        });
+    }
+
+    public void goToFirst(View view) {
+        NavDirections action = SecondFragmentDirections.actionSecondFragmentToFirstFragment();
+        Navigation.findNavController(view).navigate(action);
     }
 }
